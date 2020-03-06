b9f52ed5e17db6f5f1160c4be3f55134cdbddd21
wupeng <726451064@qq.com>
2020-03-06 19:26:49 +0800

    [add]初始化badminton-mall

---
+++ src/main/java/com/badminton/mall/controller/mall/OrderController.java
@@ -0,0 +1,140 @@
+package com.badminton.mall.controller.mall;
+
+
+import com.badminton.mall.common.Constants;
+import com.badminton.mall.common.NewBeeMallException;
+import com.badminton.mall.common.ServiceResultEnum;
+import com.badminton.mall.controller.vo.NewBeeMallOrderDetailVO;
+import com.badminton.mall.controller.vo.NewBeeMallShoppingCartItemVO;
+import com.badminton.mall.controller.vo.NewBeeMallUserVO;
+import com.badminton.mall.entity.NewBeeMallOrder;
+import com.badminton.mall.service.NewBeeMallOrderService;
+import com.badminton.mall.service.NewBeeMallShoppingCartService;
+import com.badminton.mall.util.PageQueryUtil;
+import com.badminton.mall.util.Result;
+import com.badminton.mall.util.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class OrderController {
+
+    @Resource
+    private NewBeeMallShoppingCartService newBeeMallShoppingCartService;
+    @Resource
+    private NewBeeMallOrderService newBeeMallOrderService;
+
+    @GetMapping("/orders/{orderNo}")
+    public String orderDetailPage(HttpServletRequest request, @PathVariable("orderNo") String orderNo, HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        NewBeeMallOrderDetailVO orderDetailVO = newBeeMallOrderService.getOrderDetailByOrderNo(orderNo, user.getUserId());
+        if (orderDetailVO == null) {
+            return "error/error_5xx";
+        }
+        request.setAttribute("orderDetailVO", orderDetailVO);
+        return "mall/order-detail";
+    }
+
+    @GetMapping("/orders")
+    public String orderListPage(@RequestParam Map<String, Object> params, HttpServletRequest request, HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        params.put("userId", user.getUserId());
+        if (StringUtils.isEmpty(params.get("page"))) {
+            params.put("page", 1);
+        }
+        params.put("limit", Constants.ORDER_SEARCH_PAGE_LIMIT);
+        //封装我的订单数据
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        request.setAttribute("orderPageResult", newBeeMallOrderService.getMyOrders(pageUtil));
+        request.setAttribute("path", "orders");
+        return "mall/my-orders";
+    }
+
+    @GetMapping("/saveOrder")
+    public String saveOrder(HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        List<NewBeeMallShoppingCartItemVO> myShoppingCartItems = newBeeMallShoppingCartService.getMyShoppingCartItems(user.getUserId());
+        if (StringUtils.isEmpty(user.getAddress().trim())) {
+            //无收货地址
+            NewBeeMallException.fail(ServiceResultEnum.NULL_ADDRESS_ERROR.getResult());
+        }
+        if (CollectionUtils.isEmpty(myShoppingCartItems)) {
+            //购物车中无数据则跳转至错误页
+            NewBeeMallException.fail(ServiceResultEnum.SHOPPING_ITEM_ERROR.getResult());
+        }
+        //保存订单并返回订单号
+        String saveOrderResult = newBeeMallOrderService.saveOrder(user, myShoppingCartItems);
+        //跳转到订单详情页
+        return "redirect:/orders/" + saveOrderResult;
+    }
+
+    @PutMapping("/orders/{orderNo}/cancel")
+    @ResponseBody
+    public Result cancelOrder(@PathVariable("orderNo") String orderNo, HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        String cancelOrderResult = newBeeMallOrderService.cancelOrder(orderNo, user.getUserId());
+        if (ServiceResultEnum.SUCCESS.getResult().equals(cancelOrderResult)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(cancelOrderResult);
+        }
+    }
+
+    @PutMapping("/orders/{orderNo}/finish")
+    @ResponseBody
+    public Result finishOrder(@PathVariable("orderNo") String orderNo, HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        String finishOrderResult = newBeeMallOrderService.finishOrder(orderNo, user.getUserId());
+        if (ServiceResultEnum.SUCCESS.getResult().equals(finishOrderResult)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(finishOrderResult);
+        }
+    }
+
+    @GetMapping("/selectPayType")
+    public String selectPayType(HttpServletRequest request, @RequestParam("orderNo") String orderNo, HttpSession httpSession) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        NewBeeMallOrder newBeeMallOrder = newBeeMallOrderService.getNewBeeMallOrderByOrderNo(orderNo);
+        //todo 判断订单userId
+        //todo 判断订单状态
+        request.setAttribute("orderNo", orderNo);
+        request.setAttribute("totalPrice", newBeeMallOrder.getTotalPrice());
+        return "mall/pay-select";
+    }
+
+    @GetMapping("/payPage")
+    public String payOrder(HttpServletRequest request, @RequestParam("orderNo") String orderNo, HttpSession httpSession, @RequestParam("payType") int payType) {
+        NewBeeMallUserVO user = (NewBeeMallUserVO) httpSession.getAttribute(Constants.MALL_USER_SESSION_KEY);
+        NewBeeMallOrder newBeeMallOrder = newBeeMallOrderService.getNewBeeMallOrderByOrderNo(orderNo);
+        //todo 判断订单userId
+        //todo 判断订单状态
+        request.setAttribute("orderNo", orderNo);
+        request.setAttribute("totalPrice", newBeeMallOrder.getTotalPrice());
+        if (payType == 1) {
+            return "mall/alipay";
+        } else {
+            return "mall/wxpay";
+        }
+    }
+
+    @GetMapping("/paySuccess")
+    @ResponseBody
+    public Result paySuccess(@RequestParam("orderNo") String orderNo, @RequestParam("payType") int payType) {
+        String payResult = newBeeMallOrderService.paySuccess(orderNo, payType);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(payResult)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(payResult);
+        }
+    }
+
+}
--- src/test/java/com/badminton/mall/badmintonmall/BadmintonMallApplicationTests.java
+++ src/test/java/com/badminton/mall/badmintonmall/BadmintonMallApplicationTests.java
@@ -1,7 +1,9 @@
 package com.badminton.mall.badmintonmall;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class BadmintonMallApplicationTests {
