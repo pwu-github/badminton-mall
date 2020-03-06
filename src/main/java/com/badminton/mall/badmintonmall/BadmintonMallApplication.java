08327f0c4650fa0d90acbf84ccdbaf2d11cbbfbe
wupeng <726451064@qq.com>
2020-03-06 10:40:32 +0800

    [add]创建新工程badminton-mall

---
+++ src/main/java/com/badminton/mall/badmintonmall/BadmintonMallApplication.java
@@ -0,0 +1,13 @@
+package com.badminton.mall.badmintonmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badminton/mall/badmintonmall/BadmintonMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badminton.mall.badmintonmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
